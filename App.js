790f8ca846568b95fb689557d436ce2f897d0858
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-06-14 02:52:01 -0300

    feat: initial structure

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" />
+      <Text>Strophic</Text>
+    </>
+  );
+};
+
+export default App;
